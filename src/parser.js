2a1c391e0d001ba8896b6060193a7d08b5f87c8b
Hao Guo <micguo2000@gmail.com>
2017-05-28 22:20:03 -0400

    working

--- src/parser.js
+++ src/parser.js
@@ -5,9 +5,19 @@ module.exports = {
     parse: function ($) {
         let data = [];
         $(".Results").each(function () {
+            let title = $(this).children(".MeetingCell").text();
+            let date = $(this).children(".DateCell").text();
+            let time = $(this).children(".TimeCell").text();
+            let contact = $(this).children(".ContactCell").html();
             let id = $(this).find("a[href*=\"meetid\"]").first().attr("href").match(/[0-9]{4,5}/).pop();
             if (id !== "undefined") {
-                data.push({"id": id});
+                data.push({
+                    "id": id,
+                    "title": title,
+                    "date": date,
+                    "time": time,
+                    "contact": contact
+                });
             }
         });
 
